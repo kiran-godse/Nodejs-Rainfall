109383c414ee2bd599b044f9e1a552adef9aa047
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-03 16:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ try {
   // Read the JSON file content
   const jsonContent = fs.readFileSync(jsonFilePath, 'utf8');
 
-//   // Read the schema file path from the input
-//   const schemaFilePath = core.getInput('schema-file');
+  // Read the schema file path from the input
+  const schemaFilePath = core.getInput('schema-file');
 
-//   // Read the schema file content
-//   const schemaContent = fs.readFileSync(schemaFilePath, 'utf8');
+  // Read the schema file content
+  const schemaContent = fs.readFileSync(schemaFilePath, 'utf8');
 
-//   // Perform validation using your chosen JSON schema library
-//   // Replace this section with your own validation logic
+  // Perform validation using your chosen JSON schema library
+  // Replace this section with your own validation logic
 
-//   // Log the JSON content
-//   console.log('JSON Content:', jsonContent);
+  // Log the JSON content
+  console.log('JSON Content:', jsonContent);
 
   // Set the JSON content as an output
   core.setOutput('json', jsonContent);
