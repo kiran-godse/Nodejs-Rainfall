516625684b373f065b1a89263b40a433f5bf40a7
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-04 13:03:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,40 +3,23 @@ const fs = require('fs');
 const Ajv = require('ajv');
 
 try {
-  // Read the JSON file path from the input
-  const jsonFilePath = core.getInput('json-file');
+  const recipeFilePath = core.getInput('recipe-file');
+  const recipeContent = fs.readFileSync(recipeFilePath, 'utf8');
+  const recipe = JSON.parse(recipeContent);
 
-  // Read the JSON file content
-  const jsonContent = fs.readFileSync(jsonFilePath, 'utf8');
-
-  // Read the schema file path from the input
-  const schemaFilePath = core.getInput('schema-file');
-
-  // Read the schema file content
+  const schemaFilePath = '.schema/recipe.json';
   const schemaContent = fs.readFileSync(schemaFilePath, 'utf8');
-
-  // Parse the JSON schema
   const schema = JSON.parse(schemaContent);
 
-  // Create an instance of AJV
   const ajv = new Ajv();
-
-  // Compile the schema
   const validate = ajv.compile(schema);
+  const valid = validate(recipe);
 
-  // Validate the JSON content against the schema
-  const isValid = validate(JSON.parse(jsonContent));
-
-  if (isValid) {
-    // Log the JSON content
-    console.log('JSON Content:', jsonContent);
-
-    // Set the JSON content as an output
-    core.setOutput('json', jsonContent);
+  if (!valid) {
+    core.setFailed('Recipe validation failed. Errors: ' + JSON.stringify(validate.errors));
   } else {
-    // If validation fails, set an error
-    core.setFailed('Invalid JSON file. Validation failed.');
+    core.setOutput('recipe-properties', JSON.stringify(recipe));
   }
 } catch (error) {
-  core.setFailed(`Action failed with error: ${error}`);
+  core.setFailed('An error occurred: ' + error.message);
 }
