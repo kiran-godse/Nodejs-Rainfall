588a0a591b64a48fa2d4fd4df3cce92e5a161678
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-05 18:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const Ajv2020 = require("ajv/dist/2020");
 const core = require("@actions/core");
 const fs = require("fs");
 
-const ajv = new Ajv2020();
+const ajv = new Ajv2020({ strictTypes: false });
+
+// Add the `allowUnionTypes` option to the Ajv instance
+ajv.opts.allowUnionTypes = true;
 
 try {
   // Read the JSON file path from the input
