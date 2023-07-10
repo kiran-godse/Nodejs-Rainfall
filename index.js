f16422377fac73616af1ad707868ff8763052a2b
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-10 15:18:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const Ajv2020 = require("ajv/dist/2020");
 const core = require("@actions/core");
 const fs = require("fs");
 
-const ajv = new Ajv2020({ strictTypes: false });
-
-// Add the `allowUnionTypes` option to the Ajv instance
-ajv.opts.allowUnionTypes = true;
+const ajv = new Ajv2020();
 
 try {
   // Read the JSON file path from the input
   const jsonFilePath = core.getInput("json-file");
 
-	try {
-    console.log("Registry:", registry);
-    console.log("NeedsAuth:", needsAuth);
+  // Read the JSON file content
+  const jsonContent = fs.readFileSync(jsonFilePath, "utf8");
 
+  // Read the schema file path from the input
+  const schemaFilePath = core.getInput("schema-file");
+
+  // Read the schema file content
+  const schemaContent = fs.readFileSync(schemaFilePath, "utf8");
+
+  // Compile the schema
+  const validate = ajv.compile(JSON.parse(schemaContent));
+
+  // Validate the JSON content against the schema
+  const valid = validate(JSON.parse(jsonContent));
+
+  if (valid) {
+    console.log("Validation successful");
     // Set the JSON content as an output
     core.setOutput("json", jsonContent);
   } else {
